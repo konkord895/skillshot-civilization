59b6d50cfc3517d34a1416906f29ae88333492b9
Loofiz <mak.nere2007@gmail.com>
2025-06-26 18:14:49 +0400

    truecrime

--- Abilities/small_firekiller/small_firekiller_projectile.gd
+++ Abilities/small_firekiller/small_firekiller_projectile.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+<<<<<<< Updated upstream
 var speed := 40.0
 var end_speed := 700
 var direction := Vector2.ZERO
@@ -9,6 +10,10 @@ func _ready() -> void:
 	var tween = create_tween()
 	tween.tween_property(self, "speed", end_speed, 1).set_ease(Tween.EASE_IN)
 
+=======
+var speed : float = 55
+var direction : Vector2 = Vector2.ZERO
+>>>>>>> Stashed changes
 
 func _process(delta):
 	global_position += delta * speed * direction
--- Abilities/spikes/spikes.gd
+++ Abilities/spikes/spikes.gd
@@ -1,10 +1,12 @@
 extends Ability
 
 const SPIKE = preload("res://Abilities/spikes/spike_projectile.tscn")
-
+##Кароче, я хз как мы будем называть там, где у нас типо планы расписаны, как в paint.
+##Поэтому буду называть вебсранцу "Plancheck"/"Планчик"(P.s.прикольно на и на английском, и на русском)
+##Крч чекни его, я там записал идеи по поводу spike и остальное. Время 5:11 поэтому я пошел спать
 
 func _execute(target: Vector2) -> void:
-	var spike_inst = SPIKE.instantiate()
-	spike_inst.global_position = target
-	get_node("/root/Projectiles").add_child(spike_inst)
+	var dspike_inst = SPIKE.instantiate()
+	dspike_inst.global_position = target
+	get_node("/root/Projectiles").add_child(dspike_inst)
 	pass
