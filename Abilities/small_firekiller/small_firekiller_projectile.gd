e4c61491223f28c0fc23495ca612376a6c49760d
Loofiz <mak.nere2007@gmail.com>
2025-06-28 22:28:09 +0400

    Update small_firekiller_projectile.gd

--- Abilities/small_firekiller/small_firekiller_projectile.gd
+++ Abilities/small_firekiller/small_firekiller_projectile.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-<<<<<<< Updated upstream
+
 var speed := 40.0
 var end_speed := 700
 var direction := Vector2.ZERO
@@ -10,10 +10,7 @@ func _ready() -> void:
 	var tween = create_tween()
 	tween.tween_property(self, "speed", end_speed, 1).set_ease(Tween.EASE_IN)
 
-=======
-var speed : float = 55
-var direction : Vector2 = Vector2.ZERO
->>>>>>> Stashed changes
+
 
 func _process(delta):
 	global_position += delta * speed * direction
