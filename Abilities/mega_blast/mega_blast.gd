4cbf22cacb15230326962c26da01f61ecec14235
Loofiz <mak.nere2007@gmail.com>
2025-06-29 19:22:16 +0400

    gg1

--- Abilities/mega_blast/mega_blast.gd
+++ Abilities/mega_blast/mega_blast.gd
@@ -1,9 +1,18 @@
 extends Node2D
 const MEGABLAST = preload("res://Abilities/mega_blast/mega_blast_projectile.tscn")
+@onready var act: Timer = $Act
+@onready var delet: Timer = $Delet
 
 func _execute(target: Vector2):
-	var mb_i = MEGABLAST.instantiate()
-	mb_i.direction = global_position.direction_to(target)
-	mb_i.global_position = global_position
-	Projectiles.add_child(mb_i)
+	act.start()
+	if act.time_left == 0:
+		var mb_i = MEGABLAST.instantiate()
+		mb_i.direction = global_position.direction_to(target)
+		mb_i.global_position = global_position
+		Projectiles.add_child(mb_i)
+		delet.start()
+		if delet.time_left == 0:
+			Projectiles.remove_child(mb_i)
+			
+		
 	
