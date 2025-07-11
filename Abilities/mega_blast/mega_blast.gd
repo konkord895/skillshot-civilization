b769e5428a1b998a70b5470450d9dd5df15e00cc
Loofiz <mak.nere2007@gmail.com>
2025-07-11 19:23:25 +0400

    teцфукафа

--- Abilities/mega_blast/mega_blast.gd
+++ Abilities/mega_blast/mega_blast.gd
@@ -1,15 +1,12 @@
-extends Node2D
+extends Ability
 const MEGABLAST = preload("res://Abilities/mega_blast/mega_blast_projectile.tscn")
 @onready var act: Timer = $Act
 
-func _execute(target: Vector2):
+func _execute(target: Vector2, state : State):
 	act.start()
-	if act.time_left == 0:
-		var mb_i = MEGABLAST.instantiate()
+
+func _on_act_timeout() -> void:
+	var mb_i = MEGABLAST.instantiate()
 		mb_i.direction = global_position.direction_to(target)
 		mb_i.global_position = global_position
 		Projectiles.add_child(mb_i)
-
-			
-		
-	
