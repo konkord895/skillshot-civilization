c6351a1da2f08126b37c023836537244052c0de6
Loofiz <mak.nere2007@gmail.com>
2025-07-14 19:39:05 +0400

    Gagernaott
    ggwp

--- Abilities/mega_blast/mega_blast.gd
+++ Abilities/mega_blast/mega_blast.gd
@@ -1,15 +1,15 @@
-extends Node2D
+extends Ability
 const MEGABLAST = preload("res://Abilities/mega_blast/mega_blast_projectile.tscn")
 @onready var act: Timer = $Act
 
-func _execute(target: Vector2):
-	act.start()
-	if act.time_left == 0:
-		var mb_i = MEGABLAST.instantiate()
-		mb_i.direction = global_position.direction_to(target)
-		mb_i.global_position = global_position
-		Projectiles.add_child(mb_i)
 
-			
-		
+func _execute(target: Vector2, state : State):
+	act.start()
+	cooldown.start()
+	await act.timeout and cooldown.timeout
+	var mb_i = MEGABLAST.instantiate()
+	mb_i.direction = global_position.direction_to(target)
+	mb_i.global_position = global_position
+	Projectiles.add_child(mb_i)
+	
 	
--- Abilities/mega_blast/mega_blast_projectile.gd
+++ Abilities/mega_blast/mega_blast_projectile.gd
@@ -1,9 +1,11 @@
 extends Area2D
+@onready var delet: Timer = $Delet
 
 var direction := Vector2.ZERO
 
 func _ready() -> void:
 	rotation = direction.angle()
+	delet.start()
 
 
 func _on_delet_timeout() -> void:
