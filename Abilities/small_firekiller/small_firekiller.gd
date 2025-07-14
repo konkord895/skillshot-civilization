7eb95ea73483704db6ae1bab5e96617b5f165f61
konkord16 <konkord895@gmail.com>
2025-07-14 22:32:26 +0400

    firekiller finished i guess

--- Abilities/small_firekiller/small_firekiller.gd
+++ Abilities/small_firekiller/small_firekiller.gd
@@ -1,8 +1,9 @@
 extends Ability
-const FIREKILLER = preload("res://abilities/small_firekiller/small_firekiller_projectile.tscn")
-@onready var ability_duration: Timer = $AbilityDuration
-var fire_inst: Area2D
 
+@export var rotation_speed := 20.0
+const FIREKILLER = preload("res://abilities/small_firekiller/small_firekiller_projectile.tscn")
+var fire_inst: Area2D
+@onready var ability_duration: Timer = $AbilityDuration
 
 func _execute(target: Vector2, state: State):
 	if state == State.PRESSED and ability_duration.time_left == 0: # Spawn projectile
@@ -17,6 +18,14 @@ func _execute(target: Vector2, state: State):
 		fire_inst.user = user
 		Projectiles.add_child(fire_inst)
 		ability_duration.start()
-	elif state == State.HELD:
-		fire_inst.global_position
-	
+	elif state == State.HELD and ability_duration.time_left != 0:
+		var direction_to_target := global_position.direction_to(target)
+		var target_angle := direction_to_target.angle()
+		var angle = lerp_angle(fire_inst.rotation, target_angle, 0.15)
+		fire_inst.rotation = angle
+		fire_inst.direction = Vector2.RIGHT.rotated(angle)
+
+
+func _on_ability_duration_timeout() -> void:
+	fire_inst.flying = true
+	cooldown.start()
