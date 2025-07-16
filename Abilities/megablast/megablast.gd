1c0da7be6eb046ea85eae34a167293539ad46a29
konkord16 <konkord895@gmail.com>
2025-07-16 19:19:32 +0400

    cleaning up

--- Abilities/megablast/megablast.gd
+++ Abilities/megablast/megablast.gd
@@ -1,17 +1,24 @@
 extends Ability
-const MEGABLAST = preload("res://Abilities/megablast/megablast_projectile.tscn")
 
-func _execute(target: Vector2):
+const MEGABLAST = preload("res://abilities/megablast/megablast_projectile.tscn")
+@onready var act: Timer = $Act
+
+
+func _execute(target: Vector2, state : State):
+	user.get_stunned(act.wait_time)
+	
+	cooldown.start()
+
 	var mb_i = MEGABLAST.instantiate()
+	mb_i.monitoring = false
 	mb_i.direction = global_position.direction_to(target)
 	mb_i.global_position = global_position
+	mb_i.modulate = Color(1, 1, 1, 0.1)
+	Projectiles.add_child(mb_i)
 	
-	get_node("/root/Projectiles").add_child(mb_i)
+	act.start()
+	await act.timeout
+	mb_i.monitoring = true
+	mb_i.modulate = Color(1, 1, 1, 1)
 	
-
-
-
-func _on_destoy_timeout() -> void:
-	var mb_i = MEGABLAST.instantiate()
-	mb_i.queue_free()
-	pass # Replace with function body.
+	
--- Abilities/megablast/megablast_projectile.gd
+++ Abilities/megablast/megablast_projectile.gd
@@ -1,6 +1,13 @@
 extends Area2D
+@onready var delet: Timer = $Delet
 
 var direction := Vector2.ZERO
 
 func _ready() -> void:
 	rotation = direction.angle()
+	delet.start()
+
+
+func _on_delet_timeout() -> void:
+	queue_free()
+	pass # Replace with function body.
